518ccf21bc42f160eb1800ffc29d1479b900977d
Michael-CQU <979735213@qq.com>
2021-04-24 22:07:51 +0800

    2021

--- 代码实现/代码实现/377_combinationsum4.cpp
+++ 代码实现/代码实现/377_combinationsum4.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-class Solution {
-public:
-	int combinationSum4(vector<int>& nums, int target) {
-		int n = nums.size(), sum = 0;
-		for (int i = 0; i < n; i++) {
-			if (target%nums[i] == 0)
-				sum += 1;
-		}
-	}
-};
-
-
-int main() {
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution {
+//public:
+//	int combinationSum4(vector<int>& nums, int target) {
+//		int n = nums.size(), sum = 0;
+//		for (int i = 0; i < n; i++) {
+//			if (target%nums[i] == 0)
+//				sum += 1;
+//		}
+//	}
+//};
+//
+//
+//int main() {
+//	system("pause");
+//	return 0;
+//}
