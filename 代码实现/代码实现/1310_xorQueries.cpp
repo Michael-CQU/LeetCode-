1fff2697bb210feb6816e0186d789b129b99755d
Michael-CQU <979735213@qq.com>
2021-05-12 22:04:52 +0800

    2021.5.12

--- 代码实现/代码实现/1310_xorQueries.cpp
+++ 代码实现/代码实现/1310_xorQueries.cpp
@@ -20,31 +20,32 @@ using namespace std;
 //		return xorresult;
 //	}
 //};
-class Solution {
-public:
-	vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-		int alen = arr.size();
-		vector<int> xorr (alen + 1);
-		for (int i = 0; i < alen; i++) {
-			xorr[i+1] = xorr[i] ^ arr[i];
-			//cout << xorr[i+1] << endl;
-		}
-		int qlen = queries.size();
-		vector<int> result(qlen);
-		for (int j = 0; j < qlen; j++) {
-			result[j] = xorr[queries[j][0]] ^ xorr[queries[j][1]+1];
-		}
-		return result;
-	}
-};
 
-int main() {
-	vector<int> arr = { 4,8,2,10 };
-	vector<vector<int>> queries = { {2,3},{1,3},{0,0},{0,3} };
-	Solution s;
-	vector<int> xorresult = s.xorQueries(arr, queries);
-	for (auto i : xorresult)
-		cout << i << endl;
-	system("pause");
-	return 0;
-}
+//class Solution {
+//public:
+//	vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+//		int alen = arr.size();
+//		vector<int> xorr (alen + 1);
+//		for (int i = 0; i < alen; i++) {
+//			xorr[i+1] = xorr[i] ^ arr[i];
+//			//cout << xorr[i+1] << endl;
+//		}
+//		int qlen = queries.size();
+//		vector<int> result(qlen);
+//		for (int j = 0; j < qlen; j++) {
+//			result[j] = xorr[queries[j][0]] ^ xorr[queries[j][1]+1];
+//		}
+//		return result;
+//	}
+//};
+//
+//int main() {
+//	vector<int> arr = { 4,8,2,10 };
+//	vector<vector<int>> queries = { {2,3},{1,3},{0,0},{0,3} };
+//	Solution s;
+//	vector<int> xorresult = s.xorQueries(arr, queries);
+//	for (auto i : xorresult)
+//		cout << i << endl;
+//	system("pause");
+//	return 0;
+//}
--- 代码实现/代码实现/Cpp_basics.cpp
+++ 代码实现/代码实现/Cpp_basics.cpp
@@ -90,3 +90,132 @@ using namespace std;
 //
 //	return 0;
 //}
+
+//class Cube {
+//public:
+//	void setL(int l) {
+//		m_L = l;
+//	}
+//	int getL() {
+//		return m_L;
+//	}
+//	void setW(int w) {
+//		m_W = w;
+//	}
+//	int getW() {
+//		return m_W;
+//	}
+//	void setH(int h) {
+//		m_H = h;
+//	}
+//	int getH() {
+//		return m_H;
+//	}
+//	int calculateS() {
+//		return 2 * m_L*m_W + 2*m_L*m_H + 2 * m_W*m_H;
+//	}
+//	int calculateV() {
+//		return m_L*m_H*m_W;
+//	}
+//
+//	bool isSameByClass(Cube &c) {
+//		if(m_L == c.getL() && m_H == c.getH() && m_W == c.getW())
+//			return true;
+//		return false;
+//	}
+//private:
+//	int m_L;
+//	int m_W;
+//	int m_H;
+//};
+//
+//bool isSame(Cube &c1, Cube &c2) {
+//	if (c1.getL() == c2.getL() && c1.getH() == c2.getH() && c1.getW() == c2.getW())
+//		return true;
+//	return false;
+//}
+//int main() {
+//	Cube c1;
+//	c1.setL(10);
+//	c1.setW(10);
+//	c1.setH(10);
+//
+//	Cube c2;
+//	c2.setL(10);
+//	c2.setW(10);
+//	c2.setH(10);
+//
+//	bool result = isSame(c1, c2);
+//	cout << result << endl;
+//	result = c1.isSameByClass(c2);
+//	cout << result << endl;
+//	//c1.calculateS();
+//	//c1.calculateV();
+//	system("pause");
+//	return 0;
+//}
+
+//class Point {
+//public:
+//	void setX(int x) {
+//		m_X = x;
+//	}
+//	int getX() {
+//		return m_X;
+//	}
+//	void setY(int y) {
+//		m_Y = y;
+//	}
+//	int getY() {
+//		return m_Y;
+//	}
+//private:
+//	int m_X;
+//	int m_Y;
+//};
+
+//class Circle {
+//public:
+//	void setR(int r) {
+//		m_R = r;
+//	}
+//	int getR() {
+//		return m_R;
+//	}
+//	void setCenter(Point center) {
+//		m_Center = center;
+//	}
+//	Point getCenter() {
+//		return m_Center;
+//	}
+//private:
+//	int m_R;
+//	Point m_Center;
+//};
+#include "circle.h"
+#include "point.h"
+void isInCircle(Circle &c, Point &p) {
+	int distance = (c.getCenter().getX() - p.getX()) * (c.getCenter().getX() - p.getX()) + (c.getCenter().getY() - p.getY()) * (c.getCenter().getY() - p.getY());
+	int rdistance = c.getR() * c.getR();
+	if (distance == rdistance)
+		cout << "点在圆上" << endl;
+	else if(distance > rdistance)
+		cout << "点在圆外" << endl;
+	else
+		cout << "点在圆内" << endl;
+}
+
+int main() {
+	Circle c;
+	c.setR(10);
+	Point center;
+	center.setX(10);
+	center.setY(0);
+	c.setCenter(center);
+	Point p;
+	p.setX(10);
+	p.setY(9);
+	isInCircle(c, p);
+	system("pause");
+	return 0;
+}
