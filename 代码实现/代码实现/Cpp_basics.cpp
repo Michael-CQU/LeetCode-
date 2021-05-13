c60b7d971b31655df3839849cb8d3d865ac55726
Michael-CQU <979735213@qq.com>
2021-05-13 22:07:11 +0800

    2021.5.13

--- 代码实现/代码实现/Cpp_basics.cpp
+++ 代码实现/代码实现/Cpp_basics.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 ////1. 值传递
@@ -194,28 +195,117 @@ using namespace std;
 //};
 #include "circle.h"
 #include "point.h"
-void isInCircle(Circle &c, Point &p) {
-	int distance = (c.getCenter().getX() - p.getX()) * (c.getCenter().getX() - p.getX()) + (c.getCenter().getY() - p.getY()) * (c.getCenter().getY() - p.getY());
-	int rdistance = c.getR() * c.getR();
-	if (distance == rdistance)
-		cout << "点在圆上" << endl;
-	else if(distance > rdistance)
-		cout << "点在圆外" << endl;
-	else
-		cout << "点在圆内" << endl;
-}
+//void isInCircle(Circle &c, Point &p) {
+//	int distance = (c.getCenter().getX() - p.getX()) * (c.getCenter().getX() - p.getX()) + (c.getCenter().getY() - p.getY()) * (c.getCenter().getY() - p.getY());
+//	int rdistance = c.getR() * c.getR();
+//	if (distance == rdistance)
+//		cout << "点在圆上" << endl;
+//	else if(distance > rdistance)
+//		cout << "点在圆外" << endl;
+//	else
+//		cout << "点在圆内" << endl;
+//}
+//
+//int main() {
+//	Circle c;
+//	c.setR(10);
+//	Point center;
+//	center.setX(10);
+//	center.setY(0);
+//	c.setCenter(center);
+//	Point p;
+//	p.setX(10);
+//	p.setY(9);
+//	isInCircle(c, p);
+//	system("pause");
+//	return 0;
+//}
+
+//class Person {
+//public:
+//	//无参（默认）构造函数
+//	Person() {
+//		cout << "无参构造函数!" << endl;
+//	}
+//	//有参构造函数
+//	Person(int a) {
+//		age = a;
+//		cout << "有参构造函数!" << endl;
+//	}
+//	//拷贝构造函数
+//	Person(const Person& p) {
+//		age = p.age;
+//		cout << "拷贝构造函数!" << endl;
+//	}
+//	//析构函数
+//	~Person() {
+//		cout << "析构函数!" << endl;
+//	}
+//public:
+//	int age;
+//};
+//
+////2、构造函数的调用
+////调用无参构造函数
+//void test01() {
+//	Person p; //调用无参构造函数
+//}
+//
+////调用有参的构造函数
+//void test02() {
+//
+//	//2.1  括号法，常用
+//	Person p1(10);
+//	//注意1：调用无参构造函数不能加括号，如果加了编译器认为这是一个函数声明
+//	//Person p2();
+//
+//	//2.2 显式法
+//	Person p2 = Person(10);
+//	Person p3 = Person(p2);
+//	//Person(10)单独写就是匿名对象  当前行结束之后，马上析构
+//	//Person p3;
+//
+//	//2.3 隐式转换法
+//	Person p4 = 10; // Person p4 = Person(10); 
+//	Person p5 = p4; // Person p5 = Person(p4); 
+//
+//					//注意2：不能利用 拷贝构造函数 初始化匿名对象 编译器认为是对象声明
+//					//Person p5(p4);
+//}
+//
+//int main() {
+//
+//	//test01();
+//	test02();
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+class Person {
+public:
+	Person() {
+		mA = 0;
+	}
+	//非静态成员变量占对象空间
+	int mA;
+	//静态成员变量不占对象空间
+	static int mB;
+	//函数也不占对象空间，所有函数共享一个函数实例
+	void func() {
+		cout << "mA:" << this->mA << endl;
+	}
+	//静态成员函数也不占对象空间
+	static void sfunc() {
+	}
+};
 
 int main() {
-	Circle c;
-	c.setR(10);
-	Point center;
-	center.setX(10);
-	center.setY(0);
-	c.setCenter(center);
-	Point p;
-	p.setX(10);
-	p.setY(9);
-	isInCircle(c, p);
+
+	cout << sizeof(Person) << endl;
+
 	system("pause");
+
 	return 0;
 }
