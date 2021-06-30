dab5c71f661a3f979d1eca404b05059f869c77b0
Michael-CQU <Michael-CQU@users.noreply.github.com>
2021-07-01 00:23:58 +0800

    2021.7.1

--- 代码实现/代码实现/55_2_平衡二叉树.cpp
+++ 代码实现/代码实现/55_2_平衡二叉树.cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc.h>
+#include<bits/stdc++.h>
 using namespace std;
 struct TreeNode {
 	int val;
 	TreeNode *left;
 	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-class Solution {//后序遍历，通过比较左右子树的高度差来判断。平衡二叉树必须所有子树都满足平衡条件，因此需要递归判断。
-public:
-	bool isBalanced(TreeNode* root) {
-		if (root == nullptr) return true;
-		return isBalanced(root->left) && isBalanced(root->right) && abs(depth(root->left) - depth(root->right)) < 2;
-	}
-	int depth(TreeNode* root) {
-		if (root == nullptr)return 0;
-		return max(depth(root->left), depth(root->right)) + 1;
-	}
-};
+//class Solution {//后序遍历，通过比较左右子树的高度差来判断。平衡二叉树必须所有子树都满足平衡条件，因此需要递归判断。
+//public:
+//	bool isBalanced(TreeNode* root) {
+//		if (root == nullptr) return true;
+//		return isBalanced(root->left) && isBalanced(root->right) && abs(depth(root->left) - depth(root->right)) < 2;
+//	}
+//	int depth(TreeNode* root) {
+//		if (root == nullptr)return 0;
+//		return max(depth(root->left), depth(root->right)) + 1;
+//	}
+//};
 
 //方法二：（同理）后序遍历+剪枝（效率高）
 //class Solution {
