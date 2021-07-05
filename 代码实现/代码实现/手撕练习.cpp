14925476fabc50312ec28be5b338f9eb24bb4c44
Michael-CQU <Michael-CQU@users.noreply.github.com>
2021-07-06 00:28:27 +0800

    2021.7.6

--- 代码实现/代码实现/手撕练习.cpp
+++ 代码实现/代码实现/手撕练习.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	std::vector<int> v = { 1, 2, 3, 4, 5, 6 };
+	int even_count = 0;
+	for_each(v.begin(), v.end(), [&even_count](int val) {
+		if (!(val & 1)) {
+			++even_count;
+		}
+	});
+	std::cout << "The number of even is " << even_count << std::endl;
+	system("pause");
+	return 0;
+}
