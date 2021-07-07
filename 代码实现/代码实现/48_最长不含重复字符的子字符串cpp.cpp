6a9a22740643fbaf3b9c1860f63bfc15848c53d9
Michael-CQU <979735213@qq.com>
2021-07-07 19:29:31 +0800

    2021.7.7

--- 代码实现/代码实现/48_最长不含重复字符的子字符串cpp.cpp
+++ 代码实现/代码实现/48_最长不含重复字符的子字符串cpp.cpp
@@ -10,14 +10,18 @@ public:
 	int lengthOfLongestSubstring(string s) {
 		int n = s.size(), ans = 1;
 		if (n == 0)return 0;
-		vector<int> dp(n, 1);
+		vector<int> dp(n + 1, 1);
 		unordered_map<char, int> dis;
 		dis[s[0]] = 0;
-		for (int i = 1; i < n; i++) {
-			if (i - dis[s[i - 1]] > dp[i - 1])
+		int first = -1;//注意起始位置要为-1，从而使得刚开始的几个值可以计算正确
+		for (int i = 1; i <= n; i++) {
+			if (dis.find(s[i - 1]) != dis.end()) {
+				first = dis[s[i - 1]];
+			}
+			if (i - first > dp[i - 1])//注意这里不能用等于号
 				dp[i] = dp[i - 1] + 1;
 			else
-				dp[i] = i - dis[s[i - 1]];
+				dp[i] = i - first;
 			dis[s[i - 1]] = i;
 			ans = max(ans, dp[i]);
 		}
