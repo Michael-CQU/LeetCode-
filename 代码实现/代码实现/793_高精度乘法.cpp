299f0e1f62501a6ee4929f6bffee2672b26a799b
Michael-CQU <Michael-CQU@users.noreply.github.com>
2021-07-19 23:58:32 +0800

    2021.7.19

--- 代码实现/代码实现/793_高精度乘法.cpp
+++ 代码实现/代码实现/793_高精度乘法.cpp
@@ -13,14 +13,14 @@ vector<int> mul(vector<int>&A, int b) {
 	while (C.size() > 1 && C.back() == 0)C.pop_back();
 	return C;
 }
-int main() {
-	string a;
-	int b;
-	cin >> a >> b;
-	vector<int>A;
-	for (int i = a.size() - 1; i >= 0; i--)A.push_back(a[i] - '0');
-	auto C = mul(A, b);
-	for (int i = C.size() - 1; i >= 0; i--)
-		cout << C[i];
-	return 0;
-}
+//int main() {
+//	string a;
+//	int b;
+//	cin >> a >> b;
+//	vector<int>A;
+//	for (int i = a.size() - 1; i >= 0; i--)A.push_back(a[i] - '0');
+//	auto C = mul(A, b);
+//	for (int i = C.size() - 1; i >= 0; i--)
+//		cout << C[i];
+//	return 0;
+//}
