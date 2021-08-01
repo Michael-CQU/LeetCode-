b9208084996f9e9c049138b341809db8d2c92423
Michael-CQU <979735213@qq.com>
2021-08-01 23:02:40 +0800

    2021.8.1

--- 代码实现/代码实现/93_复原IP地址.cpp
+++ 代码实现/代码实现/93_复原IP地址.cpp
@@ -35,48 +35,49 @@ using namespace std;
 //	}
 //};
 //方法二：（常规思路：掌握）
-class Solution {
-public:
-	vector<string> ans;
-	bool isvalid(string& s,int start,int end) {
-		if (start > end)return false;
-		if (s[start] == '0'&&start != end)return false;
-		int sums = 0;
-		for (int i = start; i <= end; i++) {
-			if (s[i] > '9' || s[i] < '0')return false;
-			sums = sums * 10 + (s[i] - '0');
-			if (sums > 255)
-				return false;
-		}
-		return true;
-	}
-	void generate(string& s,int idx,int pointnum) {
-		if (pointnum == 3) {
-			if (isvalid(s, idx, s.size() - 1)) {
-				ans.push_back(s);
-			}
-			return;
-		}
-			
-			for (int i = idx; i < s.size(); i++) {
-				if (isvalid(s, idx, i)) {
-					s.insert(s.begin() + i + 1, '.');
-					pointnum++;
-					generate(s, i + 2, pointnum);
-					pointnum--;
-					s.erase(s.begin() + i + 1);
-				}
-				else
-					break;
-			}
-		}
-	vector<string> restoreIpAddresses(string s) {
-		generate(s, 0, 0);
-		return ans;
-	}
-};
+//class Solution {
+//public:
+//	vector<string> ans;
+//	bool isvalid(string& s,int start,int end) {
+//		if (start > end)return false;
+//		if (s[start] == '0'&&start != end)return false;
+//		int sums = 0;
+//		for (int i = start; i <= end; i++) {
+//			if (s[i] > '9' || s[i] < '0')return false;
+//			sums = sums * 10 + (s[i] - '0');
+//			if (sums > 255)
+//				return false;
+//		}
+//		return true;
+//	}
+//	void generate(string& s,int idx,int pointnum) {
+//		if (pointnum == 3) {
+//			if (isvalid(s, idx, s.size() - 1)) {
+//				ans.push_back(s);
+//			}
+//			return;
+//		}
+//			
+//			for (int i = idx; i < s.size(); i++) {
+//				if (isvalid(s, idx, i)) {
+//					s.insert(s.begin() + i + 1, '.');
+//					pointnum++;
+//					generate(s, i + 2, pointnum);
+//					pointnum--;
+//					s.erase(s.begin() + i + 1);
+//				}
+//				else
+//					break;
+//			}
+//		}
+//	vector<string> restoreIpAddresses(string s) {
+//		generate(s, 0, 0);
+//		return ans;
+//	}
+//};
+
 //int main() {
-//	string test = "25525511135";
+//	string test = "1111";
 //	Solution s;
 //	vector<string> res = s.restoreIpAddresses(test);
 //	for (auto i : res)
