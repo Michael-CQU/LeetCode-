c44588927ffd10b1e966eb3ea8b86634368f21f3
Michael-CQU <Michael-CQU@users.noreply.github.com>
2021-08-10 12:29:41 +0800

    2021.8.10

--- 代码实现/代码实现/NC9_二叉树中是否存在节点和为指定值的路径.cpp
+++ 代码实现/代码实现/NC9_二叉树中是否存在节点和为指定值的路径.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
-	int target = 0;
-	bool res = false;
-	void mygenerate(TreeNode* root, int sum) {
-		if (root == nullptr)return;
-		target += root->val;
-		if (target == sum&&root->left == nullptr&&root->right == nullptr) {
-			res = true;//后边跟不跟return要考虑清楚，主要是需不需要进行回溯
-		}
-		if (root->left) {
-			mygenerate(root->left, sum);
-		}
-		if (root->right) {
-			mygenerate(root->right, sum);
-		}
-		target -= root->val;
-	}
-	bool hasPathSum(TreeNode* root, int sum) {
-		mygenerate(root, sum);
-		return res;
-	}
-};
+//class Solution {
+//public:
+//	int target = 0;
+//	bool res = false;
+//	void mygenerate(TreeNode* root, int sum) {
+//		if (root == nullptr)return;
+//		target += root->val;
+//		if (target == sum&&root->left == nullptr&&root->right == nullptr) {
+//			res = true;//后边跟不跟return要考虑清楚，主要是需不需要进行回溯
+//		}
+//		if (root->left) {
+//			mygenerate(root->left, sum);
+//		}
+//		if (root->right) {
+//			mygenerate(root->right, sum);
+//		}
+//		target -= root->val;
+//	}
+//	bool hasPathSum(TreeNode* root, int sum) {
+//		mygenerate(root, sum);
+//		return res;
+//	}
+//};
