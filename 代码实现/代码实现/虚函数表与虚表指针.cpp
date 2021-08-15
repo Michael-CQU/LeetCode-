611ad41b4be105b98ed6a5eecfdf3669bcefab83
Michael-CQU <979735213@qq.com>
2021-08-15 09:28:30 +0800

    2021.8.15

--- 代码实现/代码实现/虚函数表与虚表指针.cpp
+++ 代码实现/代码实现/虚函数表与虚表指针.cpp
@@ -1,31 +1,31 @@
-//#include<bits/stdc++.h>
-//using namespace std;
-//
-//class Base {  
-//public:  
-//    virtual void Bf() { cout << "Base::Bf()" << endl; }  
-//    virtual void Bg() { cout << "Base::Bg()" << endl; }  
-//    virtual void Bh() { cout << "Base::Bh()" << endl; }  
-//};  
-//
-//
-//class Derived :public Base {  
-//public:  
-//    void Bf() { cout << "Derived::Df()" << endl; }  
-//    void Dg() { cout << "Derived::Dg()" << endl; }  
-//    void Dh() { cout << "Derived::Dh()" << endl; }  
-//};  
-//
+#include<bits/stdc++.h>
+using namespace std;
+
+class Base {
+public:
+	virtual void Bf() { cout << "Base::Bf()" << endl; }
+	virtual void Bg() { cout << "Base::Bg()" << endl; }
+	virtual void Bh() { cout << "Base::Bh()" << endl; }
+};
+
+
+class Derived :public Base {
+public:
+	void Bf() { cout << "Derived::Df()" << endl; }
+	void Dg() { cout << "Derived::Dg()" << endl; }
+	void Dh() { cout << "Derived::Dh()" << endl; }
+};
+
 //int main() {
-//	Base b;
-//	Derived d;
-//	Base *m;
-//	m = &b;
-//	m->Bf();
-//	m = &d;
-//	m->Bf();
-//	m = &b;
-//	m->Bf();
+//	//Base b;
+//	//Derived d;
+//	//Base *m;
+//	//m = &b;
+//	//m->Bf();
+//	//m = &d;
+//	//m->Bf();
+//	//m = &b;
+//	//m->Bf();
 //	system("pause");
 //	return 0;
 //}
